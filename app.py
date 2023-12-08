6bcf87945af5c72a432461f208ebc9e9807c4b4a
Zombiebear99 <145797956+Zombiebear99@users.noreply.github.com>
2023-12-08 17:02:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,8 @@ import sys
 def install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
-install(sklearn)
-install(scikit-learn)
+install('sklearn')
+install('scikit-learn')
 
 # you can run your app with: streamlit run app.py
 
